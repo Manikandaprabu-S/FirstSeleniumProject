001c21d9cd3781ec3cfbe2a9d0a343afefea16d4
Manikandaprabu <massmanibrabu@gmail.com>
2021-06-11 22:27:41 +0530

    work

---
+++ src/org/last/LastIndex.java
@@ -0,0 +1,52 @@
+package org.last;
+
+public class LastIndex {
+
+	public static void main(String[] args) {
+		String m = "GreensTechnology";
+		String n = "SeleniumAutomationtool";
+		String o = "velmurugan";
+		String p = "j a v a p r o g r a m";
+		String q = "7685438890";
+
+         int a = m.lastIndexOf('o');
+         System.out.println(a);
+         
+         int b = n.lastIndexOf('o');
+         System.out.println(b);
+         
+         int c = o.lastIndexOf('n');
+         System.out.println(c);
+         
+	     int d = p.lastIndexOf(' ');
+	     System.out.println(d);
+	     
+	     int e = q.lastIndexOf('8');
+	     System.out.println(e);
+	     
+	     //Particular Character
+	     System.out.println("Particular Character");
+	     
+	     char f = m.charAt(9);
+	     System.out.println(f);
+	     
+	     char g = n.charAt(11);
+	     System.out.println(g);
+	     
+	     char h = o.charAt(4);
+	     System.out.println(h);
+	     
+	     char i = p.charAt(8);
+	     System.out.println(i);
+	     
+	     char j = q.charAt(8);
+	     System.out.println(j);
+	     
+	     
+	     
+	     
+	     
+	     
+	     
+	}	
+}
